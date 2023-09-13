6d980f25dd4ec5bf496acff7eb62800b6a04dc11
Walid Wazini <wazini25@gmail.com>
2023-09-14 00:07:17 +0800

    using Prisma

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,12 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from 'next-auth/providers/google'
 
+import prisma from "@/utils/connect"
+
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
